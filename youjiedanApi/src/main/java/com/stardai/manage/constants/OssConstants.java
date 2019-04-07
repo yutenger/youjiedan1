7182014cb8e68bb7656d832be442907d7d57fb40
yutenger <yutenger@163.com>
2019-04-07 11:10:12 +0800

    删除

--- youjiedanApi/src/main/java/com/stardai/manage/constants/OssConstants.java
+++
@@ -1,9 +0,0 @@
-package com.stardai.manage.constants;
-
-public class OssConstants {
-public final static String accessid="LTAIZuzRvLnZSpTA";
-public final static String bucket="mianze1";
-public final static String endpoint="oss-cn-hangzhou.aliyuncs.com";
-public final static String dir="youjiedan/refundImage/";
-public final static String accessKey="0I9z8XGQNGhgykEXCLaz1FYsWxhARq";
-}
